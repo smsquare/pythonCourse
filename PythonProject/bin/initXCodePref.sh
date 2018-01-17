b20d0bf36b1c4162bf7cc41a6f56b32cdac6de33
Student <student@2024-01-203-07.aii.stu>
2018-01-16 20:53:38 -0800

    Updated bin file.

--- PythonProject/bin/initXCodePref.sh
+++ PythonProject/bin/initXCodePref.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
-file="../_MAC_PYTHON_REF_/Default.xccolortheme"
+
+currDir=${PWD##*/}
+
+# Ensure that the person is running the file from the PythonProject root folderd
+dirMustBe="PythonProject"
+
+# If you are in the proper directory, then the file will be here
+file="_MAC_PYTHON_REF_/Default.xccolortheme"
 dest="/Users/student/Library/Developer/Xcode/UserData/FontAndColorThemes/Default.xccolortheme"
+
+if [ "$currDir" = "$dirMustBe" ]
+then
+	echo "curr dir: $currDir is the correct location."
+else 
+	echo "You need to be in the PythonProject directory to run this file."
+fi
+
 if [ -f "$file" ]
 then
 	echo "$file found."
-	echo "Location of Xcode:  ~/Library/Developer/Xcode/UserData/FontAndColorThemes/"
+	echo "currDir: $currDir"
 	rm $dest
 	cp -v $file $dest
 else
