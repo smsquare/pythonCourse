e6d62f6a4790447e7043c5c653cb85113609dc2a
Student <student@2024-01-203-07.aii.stu>
2018-01-16 19:56:59 -0800

    Completed shell script to upload settings

--- bin/initXCodePref.sh
+++ bin/initXCodePref.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 file="../_MAC_PYTHON_REF_/Default.xccolortheme"
+dest="/Users/student/Library/Developer/Xcode/UserData/FontAndColorThemes/Default.xccolortheme"
 if [ -f "$file" ]
 then
 	echo "$file found."
+	echo "Location of Xcode:  ~/Library/Developer/Xcode/UserData/FontAndColorThemes/"
+	rm $dest
+	cp -v $file $dest
 else
 	echo "$file not found!"
 fi
